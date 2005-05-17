f33770a0fac4abf360f8a7b02136f2577f9e946a
David Schleef <ds@schleef.org>
2005-05-17 17:59:21 +0000

    gst/librfb/rfb.h: Don't include a nonexistant header.
    Original commit message from CVS:
    * gst/librfb/rfb.h: Don't include a nonexistant header.

--- gst/librfb/rfb.h
+++ gst/librfb/rfb.h
@@ -5,7 +5,13 @@
 #include <librfb/rfbdecoder.h>
 #include <librfb/rfbbuffer.h>
 
-#include <gstrfbsrc.h>
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+GST_DEBUG_CATEGORY_EXTERN (gst_debug_rfbsrc);
+
+G_END_DECLS
 
 #endif
 
