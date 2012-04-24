23608e9203cba3a1212b33ed0d10398c871977f3
Edward Hervey <edward.hervey@collabora.co.uk>
2012-04-24 11:09:11 +0200

    dirac: Port to -base video classes

--- ext/dirac/gstdiracdec.cc
+++ ext/dirac/gstdiracdec.cc
@@ -257,15 +257,16 @@ gst_diracdec_chain (GstPad * pad, GstData * _data)
                 diracdec->decoder->seq_params.width,
                 diracdec->decoder->seq_params.height,
                 (gdouble) fps_num / (gdouble) fps_denom,
-                gst_diracdec_chroma_to_fourcc (diracdec->decoder->seq_params.
-                    chroma))) {
+                gst_diracdec_chroma_to_fourcc (diracdec->decoder->
+                    seq_params.chroma))) {
           GST_ELEMENT_ERROR (diracdec, CORE, NEGOTIATION, (NULL),
               ("Failed to set caps to %dx%d @ %d fps (format=" GST_FOURCC_FORMAT
                   "/%d)", diracdec->decoder->seq_params.width,
                   diracdec->decoder->seq_params.height,
                   diracdec->decoder->seq_params.frame_rate,
-                  gst_diracdec_chroma_to_fourcc (diracdec->decoder->seq_params.
-                      chroma), diracdec->decoder->seq_params.chroma));
+                  gst_diracdec_chroma_to_fourcc (diracdec->decoder->
+                      seq_params.chroma),
+                  diracdec->decoder->seq_params.chroma));
           c = FALSE;
           break;
         }
