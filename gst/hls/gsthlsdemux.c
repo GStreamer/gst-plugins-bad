f4dbfe947d7c085b568b58b09205aaef851896d1
Gil Pedersen <git@gpost.dk>
2012-04-25 13:31:36 +0200

    hlsdemux: start paused task on new data

--- gst/hls/gsthlsdemux.c
+++ gst/hls/gsthlsdemux.c
@@ -1159,6 +1159,7 @@ gst_hls_demux_get_next_fragment (GstHLSDemux * demux, gboolean caching)
   g_object_unref (download);
   if (!caching) {
     GST_TASK_SIGNAL (demux->updates_task);
+    gst_task_start (demux->stream_task);
   }
 
   return TRUE;
