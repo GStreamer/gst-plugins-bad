65a58130baef6c8b68f777aea137023562940c09
Edward Hervey <edward.hervey@collabora.co.uk>
2012-06-28 18:04:10 +0200

    mpegtsbase: Move known PSI PIDs to where they belong
    This avoids tsdemux parsing data is doesn't really care about

--- gst/mpegtsdemux/mpegtsparse.c
+++ gst/mpegtsdemux/mpegtsparse.c
@@ -105,6 +105,8 @@ static void mpegts_parse_release_pad (GstElement * element, GstPad * pad);
 static gboolean mpegts_parse_src_pad_query (GstPad * pad, GstQuery * query);
 static gboolean push_event (MpegTSBase * base, GstEvent * event);
 
+static void mpegts_parse_reset (MpegTSBase * base);
+
 GST_BOILERPLATE (MpegTSParse2, mpegts_parse, MpegTSBase, GST_TYPE_MPEGTS_BASE);
 
 static void
@@ -138,6 +140,7 @@ mpegts_parse_class_init (MpegTSParse2Class * klass)
   ts_class->push_event = GST_DEBUG_FUNCPTR (push_event);
   ts_class->program_started = GST_DEBUG_FUNCPTR (mpegts_parse_program_started);
   ts_class->program_stopped = GST_DEBUG_FUNCPTR (mpegts_parse_program_stopped);
+  ts_class->reset = GST_DEBUG_FUNCPTR (mpegts_parse_reset);
 }
 
 static void
@@ -147,6 +150,34 @@ mpegts_parse_init (MpegTSParse2 * parse, MpegTSParse2Class * klass)
 
   parse->srcpad = gst_pad_new_from_static_template (&src_template, "src");
   gst_element_add_pad (GST_ELEMENT (parse), parse->srcpad);
+}
+
+static void
+mpegts_parse_reset (MpegTSBase * base)
+{
+  /* Set the various know PIDs we are interested in */
+
+  /* CAT */
+  MPEGTS_BIT_SET (base->known_psi, 1);
+  /* NIT, ST */
+  MPEGTS_BIT_SET (base->known_psi, 0x10);
+  /* SDT, BAT, ST */
+  MPEGTS_BIT_SET (base->known_psi, 0x11);
+  /* EIT, ST, CIT (TS 102 323) */
+  MPEGTS_BIT_SET (base->known_psi, 0x12);
+  /* RST, ST */
+  MPEGTS_BIT_SET (base->known_psi, 0x13);
+  /* RNT (TS 102 323) */
+  MPEGTS_BIT_SET (base->known_psi, 0x16);
+  /* inband signalling */
+  MPEGTS_BIT_SET (base->known_psi, 0x1c);
+  /* measurement */
+  MPEGTS_BIT_SET (base->known_psi, 0x1d);
+  /* DIT */
+  MPEGTS_BIT_SET (base->known_psi, 0x1e);
+  /* SIT */
+  MPEGTS_BIT_SET (base->known_psi, 0x1f);
+
 }
 
 static gboolean
